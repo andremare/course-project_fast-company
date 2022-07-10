d8b174a7bbe7d0642ac7bbc737395953c383bf5d
Andrei Marenkov <103214576+andremare@users.noreply.github.com>
2022-07-10 16:08:15 +0300

    Task#1 correct-2 update

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -9,60 +9,65 @@ const Users = () => {
         setUsers(prevState => prevState.filter(user => user._id !== userId))
     }
 
-    const renderPhrase = () => {
-        return users.map(user => (
-            <tr key={user._id}>
-                <td>{user.name}</td>
-                <td>{user.qualities.map(quality => (
-                    <span className={"m-2 badge bg-" + quality.color}>
+    const renderUsersQualities = (qualities) => {
+        return (
+            qualities.map(quality => (
+                <span className={"m-2 badge bg-" + quality.color}>
                         {quality.name}
-                    </span>))}</td>
-                <td>{user.profession.name}</td>
-                <td>{user.completedMeetings}</td>
-                <td>{user.rate}</td>
-                <td>
-                    <button className="btn btn-danger"
-                            onClick={() => handleDelete(user._id)}
-                    >
-                        delete
-                    </button>
-                </td>
-            </tr>
-        ))
+                    </span>))
+        )
     }
 
-    const renderAmountTag = (amount) => {
-        const amountString = String(amount).split('')
-        const lastLetter = Number(amountString.slice(amountString.length-1, amountString.length))
-        if(amount === 0) {
+    const renderUsersTable = () => {
+        return users.map(({_id, name, qualities, profession, completedMeetings, rate}) => (
+                <tr key={_id}>
+                    <td>{name}</td>
+                    <td>{renderUsersQualities(qualities)}</td>
+                    <td>{profession.name}</td>
+                    <td>{completedMeetings}</td>
+                    <td>{rate}</td>
+                    <td>
+                        <button className="btn btn-danger"
+                                onClick={() => handleDelete(_id)}
+                        >
+                            delete
+                        </button>
+                    </td>
+                </tr>
+            )
+        )
+    }
+
+    const renderPhrase = (number) => {
+        if(!number) {
             return <h1 className="btn btn-danger m-2">Сегодня никто с тобой не тусанет</h1>
-        } else if((lastLetter === 1 && (amount === 1 || amount > 20))
-            || (amount > 4 && amount < 11)
-            || (amount > 10 && amount < 15) ) {
-            return <h1 className="btn btn-primary m-2">{amount} человек тусанет с тобой сегодня</h1>
+        } else if((number%10 === 1 && (number === 1 || number > 20))
+            || (number > 4 && number < 11)
+            || (number > 10 && number < 15) ) {
+            return <h1 className="btn btn-primary m-2">{number} человек тусанет с тобой сегодня</h1>
         } else {
-            return <h1 className="btn btn-primary m-2">{amount} человека тусанет с тобой сегодня</h1>
+            return <h1 className="btn btn-primary m-2">{number} человека тусанет с тобой сегодня</h1>
         }
     }
 
     return (
-    <>
-        {renderAmountTag(users.length)}
-        <table className="table">
-            <thead>
-            <tr>
-                <th scope="col">Имя</th>
-                <th scope="col">Качества</th>
-                <th scope="col">Профессия</th>
-                <th scope="col">Встретился,раз</th>
-                <th scope="col">Оценка</th>
-            </tr>
-            </thead>
-            <tbody>
-                 {renderPhrase()}
-            </tbody>
-        </table>
-    </>
+        <>
+            {renderPhrase(users.length)}
+            <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col">Имя</th>
+                    <th scope="col">Качества</th>
+                    <th scope="col">Профессия</th>
+                    <th scope="col">Встретился,раз</th>
+                    <th scope="col">Оценка</th>
+                </tr>
+                </thead>
+                <tbody>
+                {renderUsersTable()}
+                </tbody>
+            </table>
+        </>
     )
 }
 
