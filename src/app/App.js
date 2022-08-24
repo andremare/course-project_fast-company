3851ca73b7248f5f0dbf1720d84223fab089fcd6
Andrei Marenkov <103214576+andremare@users.noreply.github.com>
2022-08-24 20:57:48 +0300

    task-6-routing1

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Users from "./components/users";
 import NavBar from "./components/navBar";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Main from "./components/main";
 import Login from "./components/login";
 
@@ -14,6 +14,7 @@ const App = () => {
                 <Route path="/" exact component={Main} />
                 <Route path="/login" component={Login} />
                 <Route path="/users/:userId?" component={Users} />
+                <Redirect to="/" />
             </Switch>
         </div>
     );
